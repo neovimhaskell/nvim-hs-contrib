58b41c1f778abe7c71b0ed1678dc9678f55a8671
chris-bacon <chrisbacon2009@hotmail.com>
2019-03-16 10:26:50 +0000

    Remove unused imports

--- library/Neovim/User/Output.hs
+++ library/Neovim/User/Output.hs
@@ -3,9 +3,6 @@ module Neovim.User.Output where
 
 import Neovim
 import Neovim.API.String
-import Neovim.User.Choice
-
-import System.Directory
 
 -- | Invokes the substitute command with given parameters
 substitute :: (Int, Int) -> String -> String -> [String] -> Neovim env ()
